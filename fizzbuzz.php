ad888a9349a03cdd7478f6edfb6178b628dfdeda
Jake Moreman <mail@jakemoreman.co.uk>
2017-10-05 20:25:50 +0100

    Explained fizzbuzz rules.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,12 +1,16 @@
 <?php
 
 for ($i = 1; $i <= 20; $i++) {
+  # Divisible by 3 and 5
   if ((($i % 3) === 0) && (($i % 5) === 0)) {
     echo "FizzBuzz\n";
+  # Divisible by 3 only
   } else if (($i % 3) === 0) {
     echo "Fizz\n";
+  # Divisble by 5 only
   } else if (($i % 5) === 0) {
     echo "Buzz\n";
+  # Not divisible by 3 or 5
   } else {
     echo "$i\n";
   }
